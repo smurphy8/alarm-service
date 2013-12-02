239648f3d093f157bf7780c17a17144ca51fd9d6
Scott Murphy <scott@scott-VPCF226FM.(none)>
2013-12-02 15:57:56 -0600

    Working on Stateful stuff

--- src/Plow/Service/Alarm.hs
+++ src/Plow/Service/Alarm.hs
@@ -3,3 +3,24 @@ module Plow.Service.Alarm where
 -- import Plow.Service.Alarm.Internal
 
 import Plow.Service.Alarm.Types
+
+
+
+{-| Various naming conventions for stuff here I will try to stick to:
+
+Controller -> looks at the current state and decides what should happen based apon it
+
+Handler -> takes an incoming state and some parameters and returns  a new state
+
+Rxer  -> recieves some piece of data from an outside source
+
+Sender -> sends a piece of data to an outside source.
+
+Store -> put a piece of data into the local storage
+
+Get -> get a piece of data from the local storage
+
+|-}
+
+
+
